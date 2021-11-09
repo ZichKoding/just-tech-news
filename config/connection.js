54456d68f12a22bd0d66b44150af48d017d96777
Chris Zichko <chriszichkocoding@gmail.com>
2021-11-08 21:59:47 -0500

    User-model for first issue is complete, you can create, update, delete, and get all

---
+++ config/connection.js
@@ -0,0 +1,12 @@
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
+
+// create connecion to our database, pass in MySQL information for username and password
+const sequelize = new Sequelize('just_tech_news_db', 'root', 'Zooyork7!', {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+});
+
+module.exports = sequelize;
