58dfc324be7e782154b91bba332b3514ff54bf5b
Chris Zichko <chriszichkocoding@gmail.com>
2021-11-15 08:15:14 -0500

    Added password hashing with bcrypt for new and updating users

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // create connecion to our database, pass in MySQL information for username and password
-const sequelize = new Sequelize('just_tech_news_db', 'root', 'Zooyork7!', {
+const sequelize = new Sequelize('just_tech_news_db', 'root', 'ThisIsMyPasswordAndThereIsOnlyOneLikeIt', {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
