9ce5ce9143a8409a8676349f4b1105591d3a7281
Chris Zichko <chriszichkocoding@gmail.com>
2021-11-15 18:39:23 -0500

    Added bcrypt to hash password and setup the 'login' and validation that the email and password are correct

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -50,6 +50,30 @@ router.post('/', (req, res) => {
     });
 });
 
+// Validating user
+router.post('/login', (req, res) => {
+    // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if(!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address!' });
+            return;
+        }
+
+        const validatePassword = dbUserData.checkPassword(req.body.password);
+
+        if(!validatePassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+        }
+
+        res.json( {user: dbUserData, message: 'You are now logged in!'} );
+    });
+});
+
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
     // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
