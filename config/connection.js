7d4a2ac69097358df4d06df6af4622e14dddb4bc
Chris Zichko <chriszichkocoding@gmail.com>
2021-11-18 07:59:01 -0500

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,16 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-// create connecion to our database, pass in MySQL information for username and password
-const sequelize = new Sequelize('just_tech_news_db', 'root', 'ThisIsMyPasswordAndThereIsOnlyOneLikeIt', {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-});
+let sequelize;
+
+if ('https://zichko-tech-news.herokuapp.com') {
+    sequelize = new Sequelize('https://zichko-tech-news.herokuapp.com');
+} else {
+    sequelize = new Sequelize('just_tech_news_db', 'root', 'ThisIsMyPasswordAndThereIsOnlyOneLikeIt', {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+    });
+}
 
 module.exports = sequelize;
