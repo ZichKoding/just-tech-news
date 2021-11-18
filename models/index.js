1ddffe195ebd4c3b2d95ddeae168b51d7bf0f061
Chris Zichko <chriszichkocoding@gmail.com>
2021-11-18 07:35:33 -0500

    Added a Comment model with endpoints for getting all comments for a post, creating a comment, and deleting the comment

--- models/index.js
+++ models/index.js
@@ -1,4 +1,5 @@
 // import all models
+const Comment = require('./Comment');
 const Vote = require('./Vote');
 const Post = require('./Post');
 const User = require('./User');
@@ -40,4 +41,20 @@ Post.hasMany(Vote, {
   foreignKey: 'post_id'
 });
 
-module.exports = { User, Post, Vote };
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote, Comment };
---
+++ routes/api/comment-routes.js
@@ -0,0 +1,61 @@
+const router = require('express').Router();
+const { Comment, User, Post } = require('../../models');
+
+router.get('/', (req, res) => {
+    Comment.findAll({
+        attributes: [
+            'id',
+            'comment_text',
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            },
+            {
+                model: Post,
+                attributes: ['title', 'post_url']
+            }
+        ]
+    })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.json(500).json(err);
+        });
+
+});
+
+router.post('/', (req, res) => {
+    Comment.create({
+        comment_text: req.body.comment_text,
+        user_id: req.body.user_id,
+        post_id: req.body.post_id
+    })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+});
+
+router.delete('/:id', (req, res) => {
+    Comment.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbCommentData => {
+            if(!dbCommentData) {
+                res.status(400).json({ message: 'No comment found with this id' });
+                return;
+            }
+            res.json(dbCommentData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+module.exports = router;
