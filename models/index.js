4a228594fd1cfd86c7470bb62cd0b846c1aa0aa0
Chris Zichko <chriszichkocoding@gmail.com>
2021-11-17 19:15:13 -0500

    Added the upvote function as a put/create to a post and a seperate table just for votes

--- models/index.js
+++ models/index.js
@@ -1,4 +1,5 @@
 // import all models
+const Vote = require('./Vote');
 const Post = require('./Post');
 const User = require('./User');
 
@@ -11,4 +12,32 @@ Post.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Post };
+User.belongsToMany(Post, {
+  through: Vote,
+  as: 'voted_posts',
+  foreignKey: 'user_id'
+});
+
+Post.belongsToMany(User, {
+  through: Vote,
+  as: 'voted_posts',
+  foreignKey: 'post_id'
+});
+
+Vote.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Vote.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Vote, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Vote, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote };
--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,5 +1,6 @@
+const sequelize = require('../../config/connection');
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const { Post, User, Vote } = require('../../models');
 
 // get all users
 router.get('/', (req, res) => {
@@ -57,6 +58,38 @@ router.post('/', (req, res) => {
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
+    });
+});
+
+// PUT /api/posts/upvote
+router.put('/upvote', (req, res) => {
+  Vote.create({
+    user_id: req.body.user_id,
+    post_id: req.body.post_id
+  })
+    .then(() => {
+      // then find the post we just voted on
+      return Post.findOne({
+        where: {
+          id: req.body.post_id
+        },
+        attributes: [
+          'id',
+          'post_url',
+          'title',
+          'created_at',
+          // use raw MySQL aggregate function query to get a count of how many votes the post has and return it under the name 'vote_count'
+          [
+            sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
+            'vote_count'
+          ]
+        ]
+      })
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
     });
 });
 
