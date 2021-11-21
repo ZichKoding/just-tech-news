e60fe5378610b76a041ed904748bd4a3074866a0
Chris Zichko <chriszichkocoding@gmail.com>
2021-11-21 14:08:28 -0500

    Fixed with dotenv variables

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // create connecion to our database, pass in MySQL information for username and password
-const sequelize = new Sequelize('just_tech_news_db', 'root', 'ThisIsMyPasswordAndThereIsOnlyOneLikeIt', {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
