4aa0a51070ed16bbc1a852e52cb8bef6bf218e66
Chris Zichko <chriszichkocoding@gmail.com>
2021-11-22 10:44:09 -0500

    Used the {{#if}} helper to do error handling and to improve UX for if the user is logged in or out and the functionality of the comment and upvote is complete.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -30,7 +30,10 @@ router.get('/', (req, res) => {
   })
     .then(dbPostData => {
       const posts = dbPostData.map(post => post.get({ plain: true }));
-      res.render('homepage', { posts });
+      res.render('homepage', { 
+        posts,
+        loggedIn: req.session.loggedIn
+      });
     })
     .catch(err => {
       console.log(err);
