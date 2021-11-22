538e724a86b0c1a5d23a285c110703f928f5be4c
Chris Zichko <chriszichkocoding@gmail.com>
2021-11-22 16:57:07 -0500

    Added format_plural() to helper and used TDD that it works properly

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -1,7 +1,12 @@
-const { format_date } = require('../utils/helpers');
+const { format_date, format_plural } = require('../utils/helpers');
 
 test('format_date() returns a date string', () => {
     const date = new Date('2020-03-20 16:12:03');
 
     expect(format_date(date)).toBe('3/20/2020');
+});
+
+test('format_plural() returns a the word that is plural when it is needed.', () => {
+    expect(format_plural('Tiger', 2)).toBe('Tigers');
+    expect(format_plural('Lion', 1)).toBe('Lion');
 });
