652d4bce3940b0e08fe820cb21295fc92dd5bcea
Chris Zichko <chriszichkocoding@gmail.com>
2021-11-22 17:27:16 -0500

    Added a helper function for making the url more visually pleasing and tested that it will work with TDD

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -1,4 +1,4 @@
-const { format_date, format_plural } = require('../utils/helpers');
+const { format_date, format_plural, format_url } = require('../utils/helpers');
 
 test('format_date() returns a date string', () => {
     const date = new Date('2020-03-20 16:12:03');
@@ -9,4 +9,14 @@ test('format_date() returns a date string', () => {
 test('format_plural() returns a the word that is plural when it is needed.', () => {
     expect(format_plural('Tiger', 2)).toBe('Tigers');
     expect(format_plural('Lion', 1)).toBe('Lion');
+});
+
+test('format_url() returns a simplified url string', () => {
+    const url1 = format_url('http://test.com/page/1');
+    const url2 = format_url('https://www.coolstuff.com/abcdefg/');
+    const url3 = format_url('https://www.google.com?q=hello');
+
+    expect(url1).toBe('test.com');
+    expect(url2).toBe('coolstuff.com');
+    expect(url3).toBe('google.com');
 });
