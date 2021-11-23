044027b097ceb7fa36b2277563fc5b281d878015
Chris Zichko <chriszichkocoding@gmail.com>
2021-11-23 15:51:00 -0500

    ...

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3001;
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-  secret: process.env.CK_SEC,
+  secret: 'MySecret',
   cookie: {},
   resave: false,
   saveUninitialized: true,
